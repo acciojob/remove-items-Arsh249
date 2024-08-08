f30b315f7a40fd72832c1cbc6e11f21e07946405
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-08 09:17:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById('removeButton').addEventListener('click', function() {
+    var select = document.getElementById('colorSelect');
+    select.remove(select.selectedIndex);
+});
